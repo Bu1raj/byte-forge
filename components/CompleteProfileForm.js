d71ee1536a561aaa7573820ca006334cd231b26a
Arvind Awati <arvindawati2402@gmail.com>
2025-03-19 22:12:16 +0530

    assign random values to viva and exp

--- components/CompleteProfileForm.js
+++ components/CompleteProfileForm.js
@@ -39,8 +39,8 @@ export default function CompleteProfileForm() {
               status: {
                 expt01: {
                   completed: false,
-                  experimentMarks: 5,
-                  vivaMarks: 4,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -53,8 +53,8 @@ export default function CompleteProfileForm() {
 
                 expt02: {
                   completed: false,
-                  experimentMarks: 6,
-                  vivaMarks: 4,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -67,8 +67,8 @@ export default function CompleteProfileForm() {
 
                 expt03: {
                   completed: false,
-                  experimentMarks: 4,
-                  vivaMarks: 4,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks:Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -81,8 +81,8 @@ export default function CompleteProfileForm() {
 
                 expt04: {
                   completed: false,
-                  experimentMarks: 6,
-                  vivaMarks: 3,
+                  experimentMarks:Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -95,8 +95,8 @@ export default function CompleteProfileForm() {
 
                 expt05: {
                   completed: false,
-                  experimentMarks: 6,
-                  vivaMarks: 2,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -109,8 +109,8 @@ export default function CompleteProfileForm() {
 
                 expt06: {
                   completed: false,
-                  experimentMarks: 6,
-                  vivaMarks: 1,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks:Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -123,8 +123,8 @@ export default function CompleteProfileForm() {
 
                 expt07: {
                   completed: false,
-                  experimentMarks: 5,
-                  vivaMarks: 3,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -137,8 +137,8 @@ export default function CompleteProfileForm() {
 
                 expt08: {
                   completed: false,
-                  experimentMarks: 4,
-                  vivaMarks: 2,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -151,8 +151,8 @@ export default function CompleteProfileForm() {
 
                 expt09: {
                   completed: false,
-                  experimentMarks: 6,
-                  vivaMarks: 4,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks:Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
@@ -165,8 +165,8 @@ export default function CompleteProfileForm() {
                 
                 expt10: {
                   completed: false,
-                  experimentMarks: 5,
-                  vivaMarks: 2,
+                  experimentMarks: Math.floor(Math.random() * 6) + 3,
+                  vivaMarks: Math.floor(Math.random() * 4) + 1,
                   code: null,
                   numberOfSubmissions: 0,
                   vivaAns: {
